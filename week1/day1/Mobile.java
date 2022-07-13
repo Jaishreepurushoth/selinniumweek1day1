8e933b9d42e3369e2c5d17f3ee99ed88506f3923
Jaishreepurushoth <109144760+Jaishreepurushoth@users.noreply.github.com>
2022-07-13 09:40:48 +0400

    Add files via upload
    week1 day1 assessments . thank you

---
+++ week1/day1/Mobile.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			int price = 20000;
+			boolean isAndroid = true;
+			float osVersion = 11.12f;
+			char logo = 'B';
+			String brand = "OnePlus";
+			
+			System.out.println(price);
+			System.out.println(isAndroid);
+			System.out.println(osVersion);
+			System.out.println(logo);
+			System.out.println(brand);
+	}
+
+}
---
+++ week1/day1/firstcode.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class firstcode {
+
+	//main ctrl + space -> enter
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// syso ctrl + space -> enter
+		System.out.println("Welcome to TestLeaf");
+	}
+
+}
